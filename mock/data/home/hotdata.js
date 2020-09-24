1803cbcd794ddab7302da1675241fa327652c12a
xw <wzps789@163.com>
2020-09-24 21:44:01 +0800

    服务端数据

---
+++ mock/data/home/hotdata.js
@@ -0,0 +1,54 @@
+module.exports = {
+    hot1: [
+        {
+            id: Math.random().toString().slice(2),
+            title: "储物柜",
+            img: "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1730713693,2130926401&fm=26&gp=0.jpg",
+            link: "https://ss2.bdstatic.com"
+        },
+        {
+            id: Math.random().toString().slice(2),
+            title: "照明灯",
+            img: "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1730713693,2130926401&fm=26&gp=0.jpg",
+            link: "https://ss2.bdstatic.com"
+        },
+        {
+            id: Math.random().toString().slice(2),
+            title: "抱枕",
+            img: "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1730713693,2130926401&fm=26&gp=0.jpg",
+            link: "https://ss2.bdstatic.com"
+        },
+        {
+            id: Math.random().toString().slice(2),
+            title: "镜子",
+            img: "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1730713693,2130926401&fm=26&gp=0.jpg",
+            link: "https://ss2.bdstatic.com"
+        }
+    ],
+    hot2: [
+        {
+            id: Math.random().toString().slice(2),
+            title: "落地灯",
+            img: "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1730713693,2130926401&fm=26&gp=0.jpg",
+            link: "https://ss2.bdstatic.com"
+        },
+        {
+            id: Math.random().toString().slice(2),
+            title: "置物架",
+            img: "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1730713693,2130926401&fm=26&gp=0.jpg",
+            link: "https://ss2.bdstatic.com"
+        },
+        {
+            id: Math.random().toString().slice(2),
+            title: "矮脚灯",
+            img: "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1730713693,2130926401&fm=26&gp=0.jpg",
+            link: "https://ss2.bdstatic.com"
+        },
+        {
+            id: Math.random().toString().slice(2),
+            title: "毛巾",
+            img: "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1730713693,2130926401&fm=26&gp=0.jpg",
+            link: "https://ss2.bdstatic.com"
+        }
+    ]
+}
